253aa263708b5e096fc2f32c4114ba0649bf1d58
Isaac Morris <muyang13140101@gmail.com>
2026-08-28 14:13:38 +0800

    fix: configure jniLibs useLegacyPackaging for 64-bit native libraries and clean alignment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,9 @@ android {
         kotlinCompilerExtensionVersion = "1.5.8"
     }
     packaging {
+        jniLibs {
+            useLegacyPackaging = true
+        }
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
